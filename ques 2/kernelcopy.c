166690f40b21c6243f87b4d2bfaa0688a27e2199
Aanchal Gupta <108565060+aanchalg06@users.noreply.github.com>
2022-12-02 14:38:55 +0530

    Update kernelcopy.c

--- ques 2/kernelcopy.c
+++ ques 2/kernelcopy.c
@@ -12,7 +12,14 @@ int main()
 {
     long system_call_var;
     
-    printf("Matrix 1 function is: ");
+   
+
+    float *m1[][2]={{1,1},{1,1}};
+    
+    
+    float *m2[][2]={{0,0},{0,0};
+    
+     printf("Matrix 1 function is: ");
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
             printf("%f ",m1[i][j]);
@@ -20,22 +27,17 @@ int main()
         printf("\n");
     }
 
-    float *m1[][2]={{4,6},{12,6}};
-    
-    
-    float *m2[][2]={{0,0},{0,0};
-
-    system_call_var = syscall(kernel_2d_memcpy,m1,m2,2,2);
+    system_call_var = syscall(kernel_2d_memcpy,m2,m1,2,2);
 
     if (!system_call_var)
     {
         printf("451 exec done \n");
     }
-    printf("Matrix 2 function is: ");
+    printf("Matrix 1 function is: ");
                    
     for(int i=0;i<2;i++){
         for(int j=0;j<2;j++){
-            printf("%f ",m2[i][j]);
+            printf("%f ",m1[i][j]);
         }
         printf("\n");
     }
